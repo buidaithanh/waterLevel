f15191badcf1cae89916e6040d83e1d43d0ae2a0
dai thanh <thanh0985837774@gmail.com>
2023-09-20 22:28:14 +0700

    fix spell

--- routes/iotRoutes.js
+++ routes/iotRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const iotCotroller = require("../controllers/iotController");
+const iotController = require("../controllers/iotController");
 
-router.post("/distance", iotCotroller.postDistance);
-router.get("/distance", iotCotroller.getDistance);
+router.post("/distance", iotController.postDistance);
+router.get("/distance", iotController.getDistance);
 router.post("/controlPump", iotController.controlPump);
 
 module.exports = router;
