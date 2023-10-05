0bd6566d17dddaf6ae39e50a753f7d502f1acc09
dai thanh <thanh0985837774@gmail.com>
2023-10-05 17:20:41 +0700

    add validate

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,57 @@ const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
   try {
     const { username, password } = req.body;
+    // Validator for username
+    if (!username || username.trim() === "") {
+      return res.status(400).json({ error: "Username is required" });
+    }
+    if (!/^[a-zA-Z0-9]{6,20}$/.test(username)) {
+      return res.status(400).json({
+        error:
+          "Username must be 6-20 characters long, and only contain letters and digits",
+      });
+    }
+    if (/\s/.test(username)) {
+      return res
+        .status(400)
+        .json({ error: "Username must not contain spaces" });
+    }
+
+    // Validator for password
+    if (!password || password.trim() === "") {
+      return res.status(400).json({ error: "Password is required" });
+    }
+    if (password.length < 6 || password.length > 12) {
+      return res
+        .status(400)
+        .json({ error: "Password must be 6-12 characters long" });
+    }
+    if (!/[a-z]/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must contain at least one lowercase letter" });
+    }
+    if (!/[A-Z]/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must contain at least one uppercase letter" });
+    }
+    if (!/\d/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must contain at least one digit" });
+    }
+    if (!/[@$!%*?&]/.test(password)) {
+      return res.status(400).json({
+        error:
+          "Password must contain at least one special character (@, $, !, %, *, ?, &)",
+      });
+    }
+    if (/\s/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must not contain spaces" });
+    }
     const findUser = await User.findOne({ username });
     if (findUser) {
       return res.status(401).json({ error: "account exsited" });
@@ -21,10 +72,62 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { username, password } = req.body;
+    // Validator for username
+    if (!username || username.trim() === "") {
+      return res.status(400).json({ error: "Username is required" });
+    }
+    if (!/^[a-zA-Z0-9]{6,20}$/.test(username)) {
+      return res.status(400).json({
+        error:
+          "Username must be 6-20 characters long, and only contain letters and digits",
+      });
+    }
+    if (/\s/.test(username)) {
+      return res
+        .status(400)
+        .json({ error: "Username must not contain spaces" });
+    }
     const user = await User.findOne({ username });
     if (!user) {
       return res.status(401).json({ error: "account not found" });
     }
+
+    // Validator for password
+    if (!password || password.trim() === "") {
+      return res.status(400).json({ error: "Password is required" });
+    }
+    if (password.length < 6 || password.length > 12) {
+      return res
+        .status(400)
+        .json({ error: "Password must be 6-12 characters long" });
+    }
+    if (!/[a-z]/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must contain at least one lowercase letter" });
+    }
+    if (!/[A-Z]/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must contain at least one uppercase letter" });
+    }
+    if (!/\d/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must contain at least one digit" });
+    }
+    if (!/[@$!%*?&]/.test(password)) {
+      return res.status(400).json({
+        error:
+          "Password must contain at least one special character (@, $, !, %, *, ?, &)",
+      });
+    }
+    if (/\s/.test(password)) {
+      return res
+        .status(400)
+        .json({ error: "Password must not contain spaces" });
+    }
+
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ message: "password not corect" });
--- controllers/iotController.js
+++ controllers/iotController.js
@@ -9,7 +9,7 @@ const postDistance = async (req, res) => {
 // get distance from DB
 const getDistance = async (req, res) => {
   try {
-    const distances = await Distance.find().sort({ timestamp: -1 }).limit(1); // Lấy dữ liệu mới nhất
+    const distances = await Distance.find().sort({ timestamp: -1 }).limit(5); // Lấy dữ liệu mới nhất
     res.json(distances);
   } catch (error) {
     console.error(error);
